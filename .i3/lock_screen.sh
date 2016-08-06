34691e26c5789444ae19608e8bd11d7cd895795a
Akash Shende <akash0x53s@gmail.com>
2016-08-06 22:41:23 +0530

    i3 customization
    * replaced dmenu with rofi
    * changed font to OSX font
    * changed i3-bar position
    * installed infanility

--- .i3/lock_screen.sh
+++ .i3/lock_screen.sh
@@ -3,7 +3,15 @@
 #this lock the screen and turn the screen off after 90s. While inactive for 5 min 
 #it goes into suspended mode
 
-i3lock -i ~/wallpaper/anon.png
-sleep 90; pgrep i3lock && xset dpms force off
+#i3lock -i ~/wallpaper/anon.png
+#sleep 90; pgrep i3lock && xset dpms force off
 
-pgrep i3lock && sleep 300; pgrep i3lock && sudo pm-suspend
+#pgrep i3lock && sleep 300; pgrep i3lock && sudo pm-suspend
+
+scrot /tmp/screen.png
+convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+[[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
+#dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
+i3lock -f -u -i /tmp/screen.png
+rm /tmp/screen.png
+
