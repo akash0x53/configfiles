bd4ca4edbf9ec6c805b5ba092b1b64a64058f28f
Akash Shende <akash0x53s@gmail.com>
2017-01-31 11:15:11 +0530

    Few changes
    1. Change screen brightness when switch to specific workspace. [i keep
    browser on workspace 1]
    2. take photo when i logged in or any person [ paranoia ]

--- .i3/lock_screen.sh
+++ .i3/lock_screen.sh
@@ -7,11 +7,13 @@
 #sleep 90; pgrep i3lock && xset dpms force off
 
 #pgrep i3lock && sleep 300; pgrep i3lock && sudo pm-suspend
-
+take_photo(){
+	streamer -f jpeg -o "/home/akash/Pictures/who_logged_in/That_was_on_$(date +%Y-%m-%d_%H-%M).jpeg"
+}
+trap take_photo EXIT
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
-[[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
+#[[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
 #dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
-i3lock -f -u -i /tmp/screen.png -w /home/akash/.lol.png
+i3lock -f -n -u -i /tmp/screen.png -w /home/akash/.lol.png
 rm /tmp/screen.png
-
