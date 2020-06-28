793a02c1c741363965c53e83bbb02707f80da737
Akash Shende <akash0x53s@gmail.com>
2020-06-28 12:02:42 +0530

    update apt cache & install nodejs

--- setup.sh
+++ setup.sh
@@ -11,6 +11,8 @@ link_file()
 
 
 #-------- install required tools -------
+sudo apt update
+sudo apt install nodejs
 sudo apt install xsel $> /dev/null
 sudo apt install fzf
 sudo apt install silversearcher-ag #the_silver_searcher for Mac
